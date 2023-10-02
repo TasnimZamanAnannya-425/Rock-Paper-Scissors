05cc361ef2d4f59403adc13219634e80511c0c81
Tasnim Zaman <100079990+TasnimZamanAnannya-425@users.noreply.github.com>
2023-10-02 19:58:51 +0600

    Add files via upload

---
+++ Rock Paper Scissors/app.js
@@ -0,0 +1,6 @@
+const computerChoice = document.getElementById('computer-choice')
+const userChoice = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+cont possibleChoices = document.querySelectorAll('button')
+
+possibleChoices.forEach(possibleChoices)
